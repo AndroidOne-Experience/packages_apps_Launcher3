28ecb5b0694d3cf5e48d1cb7ed73c6465309aea2
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-03-15 17:08:46 +0530

    Launcher3: Show Gemini color icon while in themed mode
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- src/com/android/launcher3/qsb/QsbLayout.java
+++ src/com/android/launcher3/qsb/QsbLayout.java
@@ -98,7 +98,8 @@ public class QsbLayout extends FrameLayout implements
     private void setIcons() {
         if (Themes.isThemedIconEnabled(mContext)) {
             mAssistantIcon.setImageResource(R.drawable.ic_music_themed);
-            mGeminiIcon.setImageResource(R.drawable.ic_gemini_themed);
+            // Keep brand icon for Gemini in full color even when themed icons are enabled.
+            mGeminiIcon.setImageResource(R.drawable.ic_gemini_color);
             mGoogleIcon.setImageResource(R.drawable.ic_super_g_themed);
             mLensIcon.setImageResource(R.drawable.ic_lens_themed);
         } else {
